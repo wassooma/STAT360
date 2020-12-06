1f8a0b91a2a41f895bf5e655a7d79ac76ceefdb9
Wasim Boughattas <wassooma2013@yahoo.com>
2020-12-06 09:12:13 -0500

    added LF calculations of SLM

--- R/hello.R
+++ R/hello.R
@@ -1,32 +1,36 @@
 
 #'stat360
 #'@export
-stat_start=function(NAME, num1, num2, num3, num3b, num4, num5, multivariable=FALSE, matrix=FALSE){
-  data<- read.table(NAME,header=TRUE)
+stat_start=function(NAME, num1=0, num2=0, num3=0, num3b=0, num4=0, num5=0, multivariable=FALSE, matrix=FALSE){
+  data1<- read.table(NAME,header=TRUE)
 
   if(matrix==TRUE){
     #variables
+
     X1<<-data1$X1var
-    X2<<-0
     Yvar<<- data1$Yvar
+    n<<-length(Yvar)
+    p<<-2
+    X2<<-matrix(c(rep(0,n)),ncol=1)
+
     if(multivariable==TRUE){
       X2<<- data1$X2var
     }
-    fit <<-lm(Y~X1+X2)
-    print(summary(lm(Y~X1+X2)))
-
-    n<<-length(Yvar)
-    p<<-2
 
     Y<<-matrix(Yvar, ncol=1)
     X<<-matrix(c(rep(1,n)))
     X<<-cbind(X,X1)
-    X<<-cbind(X,X2)
+    if(multivariable==TRUE)X<<-cbind(X,X2)
 
-    J=matrix(c(rep(1,n)))
-    JJ=matrix(rep(cbind(c(),J),n), ncol=n)
+    fit<<-lm(Y~X1+X2)
+    print(summary(fit))
 
-    Xh<<-matrix(c(num3,num3b),ncol=1)
+    J<<-matrix(c(rep(1,n)))
+    JJ<<-matrix(rep(cbind(c(),J),n), ncol=n)
+    I_<<-diag(n)
+
+    Xh<<-matrix(c(1,num3),ncol=1)
+    if(multivariable==TRUE)Xh<<-matrix(c(1,num3,num3b),ncol=1)
 
     #variables assignment
     alpha<<-num1
@@ -47,8 +51,9 @@ stat_start=function(NAME, num1, num2, num3, num3b, num4, num5, multivariable=FAL
     b<<-solve(t(X)%*%X)%*%t(X)%*%Y
     b0<<-unname(b[1,1])
     b1<<-unname(b[2,1])
-    b2<<-unname(b[3,1])
-    Fitted<<-b0+b1*X1+b2*X2
+    if(multivariable==TRUE)b2<<-unname(b[3,1])
+    Fitted<<-b0+b1*X1
+    if(multivariable==TRUE)Fitted<<-b0+b1*X1+b2*X2
 
     #residuals
     H<<-X%*%solve(t(X)%*%(X))%*%t(X)
@@ -57,6 +62,10 @@ stat_start=function(NAME, num1, num2, num3, num3b, num4, num5, multivariable=FAL
 
     SSE<<-sum(residual^2)
     MSE<<-SSE/(n-2)
+
+    #########
+    s2_residual<<-MSE*(I_-H)
+    ###########
 
     # plot(X,Y)
     # lines(X,Fitted)
@@ -104,6 +113,7 @@ stat_start=function(NAME, num1, num2, num3, num3b, num4, num5, multivariable=FAL
     else print("conclude Ha")
 
     #ANOVA in R
+
     print(anova(fit))
 
     #coefficient of determination (R^2) and correlation coefficient (r)
@@ -118,10 +128,15 @@ stat_start=function(NAME, num1, num2, num3, num3b, num4, num5, multivariable=FAL
 
     #estimated variance
     s2_b<<-MSE*solve(t(X)%*%X)
+
     s2_b0<<-s2_b[1,1]
+    s_b0<<-sqrt(s2_b0)
+
     s2_b1<<-s2_b[2,2]
-    s2_b2<<-s2_b[3,3]
-    s2_b1_b2<<-(s2_b[2,3])^2
+    s_b1<<-sqrt(s2_b1)
+    if(multivariable==TRUE)s2_b2<<-s2_b[3,3]
+    if(multivariable==TRUE)s2_b1_b2<<-(s2_b[2,3])^2
+    if(multivariable==TRUE)s_b2<<-sqrt(s2_b2)
 
     #t-test concerning B1
     if(B10==0){
@@ -151,6 +166,9 @@ stat_start=function(NAME, num1, num2, num3, num3b, num4, num5, multivariable=FAL
     if(p_value>alpha) print("conclude H0 because p_value is larger than significance level: alpha")
     else print ("conclude Ha because p_value is smaller than significance level: alpha")
 
+    t_star_b1<<-t_star
+    t_table_b1<<-t_table
+    p_value_b1<<-p_value
 
     ################################################
 
@@ -191,6 +209,9 @@ stat_start=function(NAME, num1, num2, num3, num3b, num4, num5, multivariable=FAL
       else print("conclude Ha because p_value is smaller than significance level aka. alpha")
     }
 
+    t_star_b0<<-t_star
+    t_table_b0<<-t_table
+    p_value_b0<<-p_value
 
 
     ################################################
@@ -198,50 +219,62 @@ stat_start=function(NAME, num1, num2, num3, num3b, num4, num5, multivariable=FAL
     #inference on B2
 
     #confidence interval
-    upperCI_b2<<-b2+t_table*s_b2
-    lowerCI_b2<<-b2-t_table*s_b2
+    if(multivariable==TRUE){
+      upperCI_b2<<-b2+t_table*s_b2
+      lowerCI_b2<<-b2-t_table*s_b2
 
-    if(B10==0){
-      #t-test
-      t_star<<-(b2-B10)/s_b2
-      t_table<<-qt(1-alpha/2,n-p)
+      if(B10==0){
+        #t-test
+        t_star<<-(b2-B10)/s_b2
+        t_table<<-qt(1-alpha/2,n-p)
 
-      if(abs(t_star)<=t_table) print("conclude H0")
-      else print("conclude Ha")
+        if(abs(t_star)<=t_table) print("conclude H0")
+        else print("conclude Ha")
 
-      if(B10<=upperCI_b2 & B10>=lowerCI_b2) print("conclude H0 because B10 lies inside CI")
-      else print("conclude Ha because B10 does not lie inside CI")
+        if(B10<=upperCI_b2 & B10>=lowerCI_b2) print("conclude H0 because B10 lies inside CI")
+        else print("conclude Ha because B10 does not lie inside CI")
 
-      p_value<<-2*pt(-abs(t_star),n-p)
-      if(p_value>alpha) print("conclude H0 because p_value is larger than significance level aka. alpha")
-      else print("conclude Ha because p_value is smaller than significance level aka. alpha")
+        p_value<<-2*pt(-abs(t_star),n-p)
+        if(p_value>alpha) print("conclude H0 because p_value is larger than significance level aka. alpha")
+        else print("conclude Ha because p_value is smaller than significance level aka. alpha")
+      }
+      else{
+        #t-test
+        t_star<<-(b2-B10)/s_b2
+        t_table<<-qt(1-alpha,n-p)
+
+        if(abs(t_star)<=t_table) print("conclude H0")
+        else print("conclude Ha")
+
+        if(B10<=upperCI_b2 & B10>=lowerCI_b2) print("conclude H0 because B10 lies inside CI")
+        else print("conclude Ha because B10 does not lie inside CI")
+
+        p_value<<-pt(-abs(t_star),n-p)
+        if(p_value>alpha) print("conclude H0 because p_value is larger than significance level aka. alpha")
+        else print("conclude Ha because p_value is smaller than significance level aka. alpha")
+      }
     }
-    else{
-      #t-test
-      t_star<<-(b2-B10)/s_b2
-      t_table<<-qt(1-alpha,n-p)
 
-      if(abs(t_star)<=t_table) print("conclude H0")
-      else print("conclude Ha")
 
-      if(B10<=upperCI_b2 & B10>=lowerCI_b2) print("conclude H0 because B10 lies inside CI")
-      else print("conclude Ha because B10 does not lie inside CI")
-
-      p_value<<-pt(-abs(t_star),n-p)
-      if(p_value>alpha) print("conclude H0 because p_value is larger than significance level aka. alpha")
-      else print("conclude Ha because p_value is smaller than significance level aka. alpha")
-    }
-
+    t_star_b2<<-t_star
+    t_table_b2<<-t_table
+    p_value_b2<<-p_value
 
 
     ################################################
 
     #Inference on Yh_hat (interval estimation)
-    Yh_hat<<-b0+b1*Xh1+b2*Xh2
+    Yh_hat<<-b0+b1*Xh1
+    if(multivariable==TRUE)Yh_hat<<-b0+b1*Xh1+b2*Xh2
 
     #estimated variance
     s2_Yh_hat<<-MSE*t(Xh)%*%solve(t(X)%*%X)%*%Xh
     s_Yh_hat<<-sqrt(s2_Yh_hat)
+
+    #confidence interval
+    upperCI_Yh_hat<<-Yh_hat+t_table*s_Yh_hat
+    lowerCI_Yh_hat<<-Yh_hat-t_table*s_Yh_hat
+
 
     if(B10==0){
       #t-test
@@ -273,10 +306,10 @@ stat_start=function(NAME, num1, num2, num3, num3b, num4, num5, multivariable=FAL
       if(p_value>alpha) print("conclude H0 because p_value is larger than significance level aka. alpha")
       else print("conclude Ha because p_value is smaller than significance level aka. alpha")
     }
+    t_star_Yh_hat<<-t_star
+    t_table_Yh_hat<<-t_table
+    p_value_Yh_hat<<-p_value
 
-    #confidence interval
-    upperCI_Yh_hat<<-Yh_hat+t_table*s_Yh_hat
-    lowerCI_Yh_hat<<-Yh_hat-t_table*s_Yh_hat
 
     ################################################
 
@@ -364,40 +397,20 @@ stat_start=function(NAME, num1, num2, num3, num3b, num4, num5, multivariable=FAL
 
     ################################################
 
-    # #F_test_for_lack_of_fit
-    # c=nrow(data.frame(table(X)))
-    # #
-    # SSE_F_LF<<-SSPE
-    # df_F_LF<<-n-c
-    # #
-    # SSE_R_LF<<-SSE
-    # df_R_LF<<-n-2
-    # #
-    # SSLF<<-SSE-SSPE
-    # df_LF<<-c-2
-    # #
-    # MSLF<<-SSLF/(c-2)
-    # MSPE<<-SSPE/(n-c)
-    # #
-    # F_star_LF<<-MSLF/MSPE
-    # F_table_LF<<-qf(1-alpha, c-2, n-c)
-    #
-    # if(F_star<=F_table) print("conclude H0")
-    # else print("conclude Ha")
-    #
-    # Full<<-lm(Y~0 + as.factor(X))
-    # print(anova(fit,Full))
+    #F_test_for_lack_of_fit
+
+
 
     ################################################
 
     #bonferroni_inequality_to_estimate_B0_and_B1
     B_CI<<-qt(1-alpha/4,n-p)
 
-    upperCI_bonferroni_B1<<-b1+B*s_b1
-    lowerCI_bonferroni_B1<<-b1-B*s_b1
+    upperCI_bonferroni_B1<<-b1+B_CI*s_b1
+    lowerCI_bonferroni_B1<<-b1-B_CI*s_b1
 
-    upperCI_bonferroni_B0<<-b0+B*s_b0
-    lowerCI_bonferroni_B0<<-b0-B*s_b0
+    upperCI_bonferroni_B0<<-b0+B_CI*s_b0
+    lowerCI_bonferroni_B0<<-b0-B_CI*s_b0
 
     ################################################
 
@@ -413,8 +426,8 @@ stat_start=function(NAME, num1, num2, num3, num3b, num4, num5, multivariable=FAL
     #bonferroni_procedure_for_simultaneous_estimation_of_mean_response
     B_CP<<-qt(1-(alpha/(2*g)),n-p)
 
-    upperCP_bonferroni_Yh_hat<<-Yh_hat+B*s_Yh_hat
-    lowerCP_bonferroni_Yh_hat<<-Yh_hat-B*s_Yh_hat
+    upperCP_bonferroni_Yh_hat<<-Yh_hat+B_CP*s_Yh_hat
+    lowerCP_bonferroni_Yh_hat<<-Yh_hat-B_CP*s_Yh_hat
 
     ################################################
 
@@ -451,8 +464,8 @@ stat_start=function(NAME, num1, num2, num3, num3b, num4, num5, multivariable=FAL
 
   else{
     #variables
-    X<<- data$Xvar
-    Y<<- data$Yvar
+    X<<- data1$X1var
+    Y<<- data1$Yvar
     n<<-length(X)
     fit <<-lm(Y~X)
     print(summary(lm(Y~X)))
@@ -518,12 +531,14 @@ stat_start=function(NAME, num1, num2, num3, num3b, num4, num5, multivariable=FAL
     F_table<<-qf(1-alpha,1,n-2)
     if(F_star<=F_table) print("conclude H0")
     else print("conclude Ha")
+    print(paste0("for linear regression: F_star: ",F_star," F_table: ",F_table))
 
     #t-test (using F) for linear regression
     t_star_using_F<<-sqrt(F_star)
     t_table_using_F<<-qt(1-alpha/2,n-2)
     if(t_star_using_F<=t_table_using_F) print("conclude H0")
     else print("conclude Ha")
+    print(paste0("t_star_using_F: ",t_star_using_F," t_table_using_F: ",t_table_using_F))
 
     #ANOVA in R
     print(anova(fit))
@@ -531,8 +546,8 @@ stat_start=function(NAME, num1, num2, num3, num3b, num4, num5, multivariable=FAL
     #coefficient of determination (R^2) and correlation coefficient (r)
     R_2<<-SSR/SSTO
     r<<-sqrt(R_2)
-    if (b1>0) print(+r)
-    else print(-r)
+    if (b1>0) print(paste0("r: +",r))
+    else print(paste0("r: -",r))
 
     ################################################
 
@@ -548,7 +563,6 @@ stat_start=function(NAME, num1, num2, num3, num3b, num4, num5, multivariable=FAL
       t_table<<-qt(1-alpha/2,n-2)
 
       p_value<<-2*pt(-abs(t_star),n-2)
-
     }
     else{
       t_star<<-(b1-B10)/s_b1
@@ -561,6 +575,8 @@ stat_start=function(NAME, num1, num2, num3, num3b, num4, num5, multivariable=FAL
     upperCI_b1<<-b1+t_table*s_b1
     lowerCI_b1<<-b1-t_table*s_b1
 
+
+
     if(abs(t_star)<=t_table) print("conclude H0")
     else print("conclude Ha")
 
@@ -570,7 +586,9 @@ stat_start=function(NAME, num1, num2, num3, num3b, num4, num5, multivariable=FAL
     if(p_value>alpha) print("conclude H0 because p_value is larger than significance level: alpha")
     else print ("conclude Ha because p_value is smaller than significance level: alpha")
 
-
+    t_star_b1<<-t_star
+    t_table_b1<<-t_table
+    p_value_b1<<-p_value
     ################################################
 
     #inference on B0
@@ -578,6 +596,11 @@ stat_start=function(NAME, num1, num2, num3, num3b, num4, num5, multivariable=FAL
     #estimated variance
     s2_b0<<-MSE*(1/n+(mean(X))^2/Sxx)
     s_b0<<-sqrt(s2_b0)
+
+    #confidence interval
+    upperCI_b0<<-b0+t_table*s_b0
+    lowerCI_b0<<-b0-t_table*s_b0
+
 
     if(B10==0){
       #t-test
@@ -610,9 +633,9 @@ stat_start=function(NAME, num1, num2, num3, num3b, num4, num5, multivariable=FAL
       else print("conclude Ha because p_value is smaller than significance level aka. alpha")
     }
 
-    #confidence interval
-    upperCI_b0<<-b0+t_table*s_b0
-    lowerCI_b0<<-b0-t_table*s_b0
+    t_star_b0<<-t_star
+    t_table_b0<<-t_table
+    p_value_b0<<-p_value
 
     ################################################
 
@@ -622,6 +645,10 @@ stat_start=function(NAME, num1, num2, num3, num3b, num4, num5, multivariable=FAL
     #estimated variance
     s2_Yh_hat<<-MSE*(1/n+(Xh-mean(X))^2/Sxx)
     s_Yh_hat<<-sqrt(s2_Yh_hat)
+
+    #confidence interval
+    upperCI_Yh_hat<<-Yh_hat+t_table*s_Yh_hat
+    lowerCI_Yh_hat<<-Yh_hat-t_table*s_Yh_hat
 
     if(B10==0){
       #t-test
@@ -654,9 +681,9 @@ stat_start=function(NAME, num1, num2, num3, num3b, num4, num5, multivariable=FAL
       else print("conclude Ha because p_value is smaller than significance level aka. alpha")
     }
 
-    #confidence interval
-    upperCI_Yh_hat<<-Yh_hat+t_table*s_Yh_hat
-    lowerCI_Yh_hat<<-Yh_hat-t_table*s_Yh_hat
+    t_star_Yh_hat<<-t_star
+    t_table_Yh_hat<<-t_table
+    p_value_Yh_hat<<-p_value
 
     ################################################
 
@@ -703,11 +730,13 @@ stat_start=function(NAME, num1, num2, num3, num3b, num4, num5, multivariable=FAL
     df_F_general_test<<-n-2
     df_R_general_test<<-n-1
 
-    F_star_general_test<<-((SSE_R-SSE_F)/(df_R-df_F))/((SSE_F)/df_F)
+    F_star_general_test<<-((SSE_R_general_test-SSE_F_general_test)/
+                             (df_R_general_test-df_F_general_test))/
+                                ((SSE_F_general_test)/df_F_general_test)
 
-    F_table_general_test<<-qf(1-alpha, df_R-df_F, df_F)
+    F_table_general_test<<-qf(1-alpha, df_R_general_test-df_F_general_test, df_F_general_test)
 
-    if(F_star_general_test<=F_table_general_test) print("conclude H0")
+    if(F_star_general_test<=F_table_general_test) print("general test approach: conclude H0")
     else print("conclude Ha")
 
     ################################################
@@ -739,30 +768,60 @@ stat_start=function(NAME, num1, num2, num3, num3b, num4, num5, multivariable=FAL
     #else print("Emperical rule not respected")
 
     plot(sort(Ee),sort(residual))
-
+    print("FIT2")
     fit2<- lm(sort(residual) ~ sort(Ee))
-    anova(fit2)
-    summary(fit2)
+    # print(anova(fit2))
+    # print(summary(fit2))
 
     ################################################
 
     #F_test_for_lack_of_fit
-    c=nrow(data.frame(table(X)))
-    #
-    SSE_F_LF<<-SSPE
-    df_F_LF<<-n-c
-    #
-    SSE_R_LF<<-SSE
-    df_R_LF<<-n-2
-    #
-    SSLF<<-SSE-SSPE
-    df_LF<<-c-2
-    #
-    MSLF<<-SSLF/(c-2)
-    MSPE<<-SSPE/(n-c)
-    #
+    data_table<<-data.frame(x=X,y=Y,nj=c(rep(1,n)),
+                          mean=c(rep(0,n)),stDev=c(rep(NA,n)))
+
+    for(i in 1:nrow(data_table)){
+      if(data_table[i,1]==0)next
+      for(j in i:nrow(data_table)){
+        if(i==j) next
+        if(data_table[j,1]==data_table[i,1]) {
+
+          data_table[i,2]<<-paste0(data_table[i,2],", ",data_table[j,2])
+          data_table[i,3]<<-data_table[i,3]+1
+
+          data_table[j,]<<-0
+        }
+      }
+    }
+
+    data_table<<-subset(data_table,x!=0)
+    rownames(data_table) <<- NULL
+
+    c_<<-nrow(data_table)
+
+    for(i in 1:nrow(data_table)){
+      v_=c()
+      for(j in 1:data_table[i,3]){
+        v_=cbind(v_,as.double(tokenize_string(data_table[i,2])
+                              [1+(j-1)*3,1]))
+        data_table[i,4]<<-mean(v_)
+        data_table[i,5]<<-sd(v_)
+      }
+    }
+
+    data_table[is.na(data_table)]<<-0
+    print(data_table)
+
+    SSPE<<-0
+    for(i in 1:nrow(data_table)){
+      SSPE<<-SSPE+data_table[i,5]^2*(data_table[i,3]-1)
+    }
+
+    MSPE<<-SSPE/(n-c_)
+    SSLF<<-(SSE-SSPE)
+    MSLF<<-SSLF/(c_-2)
+
     F_star_LF<<-MSLF/MSPE
-    F_table_LF<<-qf(1-alpha, c-2, n-c)
+    F_table_LF<<-qf(1-alpha, c_-2, n-c_)
 
     if(F_star<=F_table) print("conclude H0")
     else print("conclude Ha")
@@ -775,11 +834,11 @@ stat_start=function(NAME, num1, num2, num3, num3b, num4, num5, multivariable=FAL
     #bonferroni_inequality_to_estimate_B0_and_B1
     B_CI<<-qt(1-alpha/4,n-2)
 
-    upperCI_bonferroni_B1<<-b1+B*s_b1
-    lowerCI_bonferroni_B1<<-b1-B*s_b1
+    upperCI_bonferroni_B1<<-b1+B_CI*s_b1
+    lowerCI_bonferroni_B1<<-b1-B_CI*s_b1
 
-    upperCI_bonferroni_B0<<-b0+B*s_b0
-    lowerCI_bonferroni_B0<<-b0-B*s_b0
+    upperCI_bonferroni_B0<<-b0+B_CI*s_b0
+    lowerCI_bonferroni_B0<<-b0-B_CI*s_b0
 
     ################################################
 
@@ -801,8 +860,8 @@ stat_start=function(NAME, num1, num2, num3, num3b, num4, num5, multivariable=FAL
     s2_Yh_hat<<-MSE*(1/n+(Xh-mean(X))^2/Sxx)
     s_Yh_hat<<-sqrt(s2_Yh_hat)
 
-    upperCP_bonferroni_Yh_hat<<-Yh_hat+B*s_Yh_hat
-    lowerCP_bonferroni_Yh_hat<<-Yh_hat-B*s_Yh_hat
+    upperCP_bonferroni_Yh_hat<<-Yh_hat+B_CP*s_Yh_hat
+    lowerCP_bonferroni_Yh_hat<<-Yh_hat-B_CP*s_Yh_hat
 
     ################################################
 
